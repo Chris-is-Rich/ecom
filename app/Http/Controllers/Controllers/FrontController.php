593f258342687385ad2804c6e7bce8771a0d3b9d
Chris-is-Rich <ichriskelly12@gmail.com>
2017-08-03 19:12:28 -0700

    Add files via upload

---
+++ app/Http/Controllers/Controllers/FrontController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Product;
+
+
+class FrontController extends Controller
+{
+    //
+    public function index()
+    {
+        $shirts=Product::all();
+        return view('front.home',compact('shirts'));
+    }
+
+    public function shirts()
+    {
+      $shirts=Product::all();
+      return view('front.shirts',compact('shirts'));
+    }
+    public function shirt($id)
+    {
+      $shirts=Product::all();
+      $products=Product::find($id);
+      return view('front.shirt',compact('shirts'),compact('products'));
+    }
+
+}
